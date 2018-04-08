9719b542e00d29effced9843d89f9a6b9756a4fc
parry <331940353@qq.com>
2018-04-08 09:13:05 +0800

    delete glide

--- mdragvideo/build/generated/source/buildConfig/debug/com/miaoyongjun/mdragvideo/BuildConfig.java
+++ mdragvideo/build/generated/source/buildConfig/debug/com/miaoyongjun/mdragvideo/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.miaoyongjun.mdragvideo";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 12;
-  public static final String VERSION_NAME = "1.2";
+  public static final int VERSION_CODE = 15;
+  public static final String VERSION_NAME = "1.5";
 }
